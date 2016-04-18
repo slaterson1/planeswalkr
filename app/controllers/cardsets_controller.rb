9caff77cfa95fea0444c1076765e76437eea596f
Brit Butler <redline6561@gmail.com>
2016-04-18 15:29:26 -0400

    Add a CardSet import method.

--- app/controllers/cardsets_controller.rb
+++ app/controllers/cardsets_controller.rb
@@ -1,9 +1,42 @@
 class CardsetsController < ApplicationController
-  before_action :authenticate!, except: [:show]
+  before_action :administrate!, except: [:show]
 
   def new
+    render :new
   end
 
   def create
+    file = params[:cardset]
+    begin
+      data = JSON.parse(file.read)
+    rescue JSON::ParserError => e
+      flash[:notice] = "That JSON file is all fucked up."
+      redirect_to root_path
+    end
+    ActiveRecord::Base.transaction do
+      @set = CardSet.new(name: data["name"],
+                         set_type: data["type"],
+                         code: data["code"],
+                         release_date: DateTime.parse(data["releaseDate"]),
+                         block: data["block"])
+      @cards = data["cards"].map do |card|
+        Card.new(name: card["name"],
+                 mana_cost: card["manaCost"],
+                 converted_cost: card["cmc"],
+                 card_type: card["type"],
+                 rarity: card["rarity"],
+                 text: card["text"],
+                 flavor: card["flavor"],
+                 artist: card["artist"],
+                 power: card["power"],
+                 toughness: card["toughness"],
+                 card_number: card["mciNumber"],
+                 colors: card["colors"])
+      end
+      @set.cards = @cards
+      @set.save
+    end
+    flash[:notice] = "New Set Import #{@set.name} was successful." if @set.persisted?
+    redirect_to root_path
   end
 end
