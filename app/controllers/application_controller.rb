ea182e30a59d0d970df566ef026b74d94b9bd314
Brit Butler <redline6561@gmail.com>
2016-04-18 14:51:45 -0400

    Fix minor buglet in current_user method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    user_id = sessions[:user_id]
+    user_id = session[:user_id]
     user_id && User.find_by(id: user_id)
   end
 
