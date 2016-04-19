5c39deb6b3b572528e56e36b8e149791e41ef54b
Brit Butler <redline6561@gmail.com>
2016-04-18 23:33:33 -0400

    Add image_dir to CardSets.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418184538) do
+ActiveRecord::Schema.define(version: 20160419033227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160418184538) do
     t.string   "block"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "image_dir"
   end
 
   create_table "cards", force: :cascade do |t|
