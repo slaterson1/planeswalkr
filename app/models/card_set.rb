a31c4d5bfd368be250312669d4b0b7f6cc6e0c9f
tallkeith <tallestkeith@gmail.com>
2016-04-20 15:03:09 -0400

    removed multisearch from card_sets

--- app/models/card_set.rb
+++ app/models/card_set.rb
@@ -1,7 +1,4 @@
 class CardSet < ActiveRecord::Base
   has_many :cards
 
-  include PgSearch
-  multisearchable :against => [ :name, :set_type, :code, :release_date,
-                         				:block, :image_dir ]
 end
