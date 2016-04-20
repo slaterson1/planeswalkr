0aa505525de78d54538af864c118298ff54c2c3c
jbudrow <jbudrowd3@yahoo.com>
2016-04-20 15:30:53 -0400

    resources route for deck model

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   delete "/login", to: "sessions#destroy"
 
   resources :cardsets, only: [:new, :create, :index, :show]
+  resources :decks 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
