5869d76448961d271f70c5100651e250ccab0ac8
Jennifer Ford <jenncford@gmail.com>
2016-04-20 15:36:14 -0400

    added routes for DeckCard via resources

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   delete "/login", to: "sessions#destroy"
 
   resources :cardsets, only: [:new, :create, :index, :show]
+  resources :deck_cards
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
