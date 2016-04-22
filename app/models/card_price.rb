83841208990d7b7bc28385bd80cc0d58822d07c7
slatersan1 <cdlee212@gmail.com>
2016-04-22 10:11:30 -0400

    add Nokogiri gem, attempt to implement web scraping for card prices

---
+++ app/models/card_price.rb
@@ -0,0 +1,21 @@
+# PriceAPI.get_price(@card)
+
+require 'nokogiri'
+require 'httparty'
+require 'open-uri'
+
+class Prices	
+
+	def get_set_url
+		set_name = card_sets.name
+		set_html = HTTParty.get(http://mtgjson.com/sets/"#{'set_name'}".html)
+		# LOST!!!!!!!!! HELP MEEEEE!!!!!!!!!!!!
+	end	
+
+	def get_price(card)
+		number = card.mciNumber
+		card_html = HTTParty.get(http://www.magiccards.info/al/en/"#{'mciNumber'}".html)
+		price = Nokogiri::HTML(card_html)
+		price.xpath("//td[@class='TCGPHiLoMid']")
+	end
+end	
